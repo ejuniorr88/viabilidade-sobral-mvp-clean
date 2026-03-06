d6f4b0811ac951c3ec23b92ee458136c8d05fa5b
ejuniorr88 <ejuniorrrr@gmail.com>
2026-03-06 16:00:03 -0300

    Update __init__.py

--- ui/relatorio_blocks/__init__.py
+++ ui/relatorio_blocks/__init__.py
@@ -6,16 +6,7 @@ Este pacote expõe funções de renderização usadas pelo ui/relatorio.py.
 from .quadro_tecnico import render_quadro_tecnico
 from .dicas_valiosas import render_dicas_valiosas
 from .figuras_anexo_v import render_figuras_anexo_v
-
-# Multifamiliar (Fase 1 - Guia)
-try:
-    from .multifamiliar_guia import render_multifamiliar_guia
-except Exception as e:  # pragma: no cover
-    # Mantém import do app explícito e com erro amigável caso o módulo não exista
-    def render_multifamiliar_guia(*args, **kwargs):
-        raise ImportError(
-            "Não foi possível importar render_multifamiliar_guia de ui/relatorio_blocks/multifamiliar_guia.py"
-        ) from e
+from .multifamiliar_guia import render_multifamiliar_guia
 
 __all__ = [
     "render_quadro_tecnico",
