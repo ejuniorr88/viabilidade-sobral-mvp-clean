608c57d6b9034d2ee074c5a0682a919fa9233e65
ejuniorr88 <ejuniorrrr@gmail.com>
2026-03-12 19:18:56 -0300

    Update config.js

--- auth_external/auth_frontend/config.js
+++ auth_external/auth_frontend/config.js
@@ -2,6 +2,6 @@ window.AUTH_CONFIG = {
   SUPABASE_URL: "https://dvaskwtqrohfyzndtjwv.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_2OlRW3Br1i3EcuDoTyOB3A_NoPs8pR-",
   GATEWAY_BASE_URL: "https://viabilidade-auth-gateway.onrender.com",
-  LOGIN_REDIRECT_URL: "https://viabilidade-sobral-mvp-clean-qozxvkfct-ejuniorr88s-projects.vercel.app",
+  LOGIN_REDIRECT_URL: "https://viabilidade-sobral-mvp-clean.vercel.app/",
   STREAMLIT_APP_URL: "https://viabilidadeteste.streamlit.app"
 };
