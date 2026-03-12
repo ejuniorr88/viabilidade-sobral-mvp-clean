26f45c912dede13d23cf8965a37e0780ccecea13
ejuniorr88 <ejuniorrrr@gmail.com>
2026-03-12 16:41:26 -0300

    Update config.js

--- auth_external/auth_frontend/config.js
+++ auth_external/auth_frontend/config.js
@@ -1,6 +1,6 @@
 window.AUTH_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://dvaskwtqrohfyzndtjwv.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_2OlRW3Br1i3EcuDoTyOB3A_NoPs8pR-",
   GATEWAY_BASE_URL: "http://127.0.0.1:8000",
   LOGIN_REDIRECT_URL: "http://127.0.0.1:3000",
   STREAMLIT_APP_URL: "https://viabilidadeteste.streamlit.app"
