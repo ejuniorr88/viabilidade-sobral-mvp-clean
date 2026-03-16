6d200286009751d94cbe99cce301bb8d1fd73171
ejuniorr88 <ejuniorrrr@gmail.com>
2026-03-16 16:24:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -559,7 +559,7 @@ if run_free_calc_now:
 
     if calc.get("zone") and not calc.get("rule"):
         try:
-            rule = fetch_rule(calc.get("zone_lookup") or calc.get("zone_sigla") or calc["zone"], calc.get("use_type_code") or "RES_UNI", calc.get("subzone_code") or "PADRAO", calc.get("zone_label_raw") or calc.get("zone"))
+            rule = fetch_rule(calc.get("zone_sigla") or calc["zone"], calc.get("use_type_code") or "RES_UNI", calc.get("subzone_code") or "PADRAO", calc.get("zone_label_raw") or calc.get("zone"))
             if rule:
                 calc["rule"] = rule
                 st.session_state.free_calc_done = True
@@ -574,15 +574,17 @@ if run_free_calc_now:
 elif show_item3:
     _ = render_localizacao_section(False, zones_prepared, radius_m)
 
-if st.session_state.get("free_calc_done"):
+if calc.get("zone"):
     render_indices_section(
         calc=calc,
         card_func=_card,
         pick_func=pick_rule,
         get_rule_func=fetch_rule,
     )
+    if calc.get("rule") and not calc.get("err"):
+        st.session_state.free_calc_done = True
 
-can_offer_report = bool(st.session_state.get("free_calc_done")) and bool(calc.get("zone")) and not bool(calc.get("err"))
+can_offer_report = bool(st.session_state.get("free_calc_done")) and bool(calc.get("zone")) and not bool(calc.get("err")) and bool(calc.get("rule"))
 
 if can_offer_report:
     st.markdown("---")
