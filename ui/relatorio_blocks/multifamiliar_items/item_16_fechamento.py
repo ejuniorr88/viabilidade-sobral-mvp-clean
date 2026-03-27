98ef509ee76537fd4795eb8cfa881e54a76500e0
ejuniorr88 <ejuniorrrr@gmail.com>
2026-03-27 07:24:42 -0300

    Create item_16_fechamento.py

---
+++ ui/relatorio_blocks/multifamiliar_items/item_16_fechamento.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from . import common
+
+def render(ctx):
+    common.st.markdown(
+        "**Este relatório foi pensado para ajudar você a entender o terreno de forma mais simples.**\n\n**Na etapa de projeto e aprovação, ainda será preciso conferir os detalhes completos no licenciamento.**"
+    )
