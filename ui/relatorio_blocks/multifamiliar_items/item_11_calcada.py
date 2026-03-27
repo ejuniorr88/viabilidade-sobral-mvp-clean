c33342551bac2f0c235740bfeca1ed7cede2b271
ejuniorr88 <ejuniorrrr@gmail.com>
2026-03-27 07:00:36 -0300

    Delete ui/relatorio_blocks/multifamiliar_items/item_11_calcada.py

--- ui/relatorio_blocks/multifamiliar_items/item_11_calcada.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-from . import common
-from ..figuras_anexo_v import render_figuras_anexo_v
-
-def render(ctx):
-    common.st.markdown("**A análise do terreno não termina dentro do lote. Também existem regras para calçada, acesso ao imóvel, rebaixo de meio-fio e relação com a rua.**")
-    render_figuras_anexo_v(ctx['rule'] or {}, is_corner=bool(common.st.session_state.get('lot_is_corner') or ctx['calc'].get('lot_is_corner')))
