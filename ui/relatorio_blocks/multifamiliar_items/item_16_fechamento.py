dcaff738c3b5a26649120c3b5315e7efe28be73c
ejuniorr88 <ejuniorrrr@gmail.com>
2026-03-27 08:24:40 -0300

    Update item_16_fechamento.py

--- ui/relatorio_blocks/multifamiliar_items/item_16_fechamento.py
+++ ui/relatorio_blocks/multifamiliar_items/item_16_fechamento.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 from . import common
 
+
 def render(ctx):
     common.st.markdown(
-        "**Este relatório foi pensado para ajudar você a entender o terreno de forma mais simples.**\n\n**Na etapa de projeto e aprovação, ainda será preciso conferir os detalhes completos no licenciamento.**"
+        "Este relatório foi pensado para ajudar a entender o terreno de forma mais simples.\n\n"
+        "Na etapa de projeto e aprovação, ainda será preciso conferir os detalhes completos no setor de licenciamento de obras da prefeitura."
     )
