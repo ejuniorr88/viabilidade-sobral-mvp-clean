819b8c4b57e98b3cdfc6d91646f1baf47a6e7e99
ejuniorr88 <ejuniorrrr@gmail.com>
2026-04-01 16:30:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ except Exception:
 from ui.mapa import render_mapa_section
 from ui.lot.inputs import render_lot_inputs
 from ui.location.section import render_localizacao_section
-from ui.indices import render_indices_section
-from ui.analise import render_analise_section
+from ui.indices.section import render_indices_section
+from ui.analysis.section import render_analise_section
 from ui.report.section import render_report_section
 from ui.relatorio import (
     render_relatorio_section,
