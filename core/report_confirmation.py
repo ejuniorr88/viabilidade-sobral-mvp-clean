ec02fe580ccc832e7bcbe2d07ec4812130b02a43
ejuniorr88 <ejuniorrrr@gmail.com>
2026-04-07 21:47:27 -0300

    Update report_confirmation.py

--- core/report_confirmation.py
+++ core/report_confirmation.py
@@ -5,16 +5,6 @@ from copy import deepcopy
 from typing import Any, Callable, Dict, MutableMapping
 
 
-PENDING_REPORT_STATE_KEYS = (
-    "confirm_new_report",
-    "pending_report_calc",
-    "pending_report_session",
-    "pending_report_signature",
-    "report_review_open",
-    "report_terms_accepted",
-)
-
-
 def _safe_float(value: Any) -> float:
     try:
         return float(value)
@@ -94,7 +84,6 @@ def clear_pending_report(session_state: MutableMapping[str, Any]) -> None:
     session_state["pending_report_session"] = None
     session_state["pending_report_signature"] = None
     session_state["report_review_open"] = False
-    session_state["report_terms_accepted"] = False
 
 
 def clear_report_runtime_state(
@@ -190,17 +179,11 @@ def arm_report_review(
 ) -> None:
     clear_pending_report(session_state)
     session_state["report_review_open"] = True
-    session_state["report_terms_accepted"] = False
     session_state["pending_report_calc"] = deepcopy(calc_ref)
     session_state["pending_report_session"] = deepcopy(current_report_session)
     session_state["pending_report_signature"] = current_report_signature
     if requires_new_credit:
-        arm_new_report_confirmation(
-            session_state=session_state,
-            calc_ref=calc_ref,
-            current_report_session=current_report_session,
-            current_report_signature=current_report_signature,
-        )
+        session_state["confirm_new_report"] = True
 
 
 def should_reset_pending_review(
