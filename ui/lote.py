67df0c5ad3203eda2b8784a8c060b26f927427ed
ejuniorr88 <ejuniorrrr@gmail.com>
2026-05-05 09:03:50 -0300

    Update lote.py

--- ui/lote.py
+++ ui/lote.py
@@ -191,7 +191,7 @@ def render_lote_section() -> Tuple[float, float, float]:
     )
     st.markdown(
     """
-    <div style="font-size: 12px; color: #666; line-height: 1.4; margin-top: -6px; margin-bottom: 6px;">
+    <div style="font-size: 12px; color: #666; line-height: 2.0; margin-top: -6px; margin-bottom: 6px;">
         Observação: Se ainda não souber a área construída no térreo, deixe 0 para calcular o potencial máximo permitido.
     </div>
     """,
